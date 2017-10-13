7f2c3822314241c764d1cb67fb53d5285e9ab1e0
tscott-au <tim@nepelle.com.au>
2017-10-13 11:56:26 +1100

    after mix new

---
+++ lib/frdomain_elixir.ex
@@ -0,0 +1,18 @@
+defmodule FRDomain do
+  @moduledoc """
+  Documentation for FRDomain.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FRDomain.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/frdomain_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule FRDomainTest do
+  use ExUnit.Case
+  doctest FRDomain
+
+  test "greets the world" do
+    assert FRDomain.hello() == :world
+  end
+end
